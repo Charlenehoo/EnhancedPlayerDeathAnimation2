e882b8af01ad5cf9d31a43013d6015aa1005e68d
CharleneHoo <CharleneHoo@hotmail.com>
2026-04-18 11:53:43 +0800

    命名空间

--- lua/autorun/server/life_cycle.lua
+++ lua/autorun/server/life_cycle.lua
@@ -31,11 +31,11 @@ local function setMaxHealthByState(ply, state)
 end
 
 -- 核心函数：在 ScalePlayerDamage 阶段处理潜在致命伤害（仅在 damage > ply:Health() 时调用）
--- 副作用：临时提高生命上限、补充血量、更新 ply.context.state
+-- 副作用：临时提高生命上限、补充血量、更新 ply.EPDAContext.state
 local function handlePotentialDeath(ply, damage)
     local totalBonus = 0
     local currentHealth = ply:Health()
-    local currentState = ply.context.state
+    local currentState = ply.EPDAContext.state
 
     -- 递推兑现：使用映射表
     local transition = StateTransition[currentState]
@@ -48,27 +48,27 @@ local function handlePotentialDeath(ply, damage)
     -- 临时提高生命上限并补充血量（引擎随后会扣除 damage）
     ply:SetMaxHealth(ply:GetMaxHealth() + totalBonus)
     ply:SetHealth(currentHealth + totalBonus)
-    ply.context.state = currentState
+    ply.EPDAContext.state = currentState
 end
 
 local function handlePlayerTakeDamage(ply, dmginfo)
     local damage = dmginfo:GetDamage()
     if damage > ply:Health() then
-        if not ply.context then
-            ply.context = { state = State.COMBAT }
+        if not ply.EPDAContext then
+            ply.EPDAContext = { state = State.COMBAT }
         end
 
         handlePotentialDeath(ply, damage)
 
-        if not ply.context.hasTriggeredFakeDeath then
-            ply.context.hasTriggeredFakeDeath = true
+        if not ply.EPDAContext.hasTriggeredFakeDeath then
+            ply.EPDAContext.hasTriggeredFakeDeath = true
             local props = CreateProps(ply)
-            ply.context.ragdoll = props.ragdoll
-            ply.context.animator = props.animator
-            ply.context.follower = props.follower
+            ply.EPDAContext.ragdoll = props.ragdoll
+            ply.EPDAContext.animator = props.animator
+            ply.EPDAContext.follower = props.follower
 
             net.Start("EPDA_Ragdoll")
-            net.WriteEntity(ply.context.ragdoll)
+            net.WriteEntity(ply.EPDAContext.ragdoll)
             net.Send(ply)
 
             ply:SetModelScale(0.1, 0)
@@ -86,10 +86,10 @@ hook.Add("EntityTakeDamage", "EntityTakeDamage_EPDA_LifeCycle", function(target,
 end)
 
 local function handlePostPlayerTakeDamage(ply, dmginfo, wasDamageTaken)
-    if not ply.context then return end -- 未初始化，说明玩家处于 State.COMBAT，无需干预
-    if not ply:Alive() then return end -- 玩家已死亡，无需干预
+    if not ply.EPDAContext then return end -- 未初始化，说明玩家处于 State.COMBAT，无需干预
+    if not ply:Alive() then return end     -- 玩家已死亡，无需干预
 
-    setMaxHealthByState(ply, ply.context.state)
+    setMaxHealthByState(ply, ply.EPDAContext.state)
 end
 
 hook.Add("PostEntityTakeDamage", "PostEntityTakeDamage_EPDA_LifeCycle", function(ent, dmginfo, wasDamageTaken)
@@ -102,16 +102,16 @@ hook.Add("PostEntityTakeDamage", "PostEntityTakeDamage_EPDA_LifeCycle", function
 end)
 
 hook.Add("PostPlayerDeath", "PostPlayerDeathe_EPDA_LifeCycle", function(ply)
-    if not ply.context then return end
+    if not ply.EPDAContext then return end
 
-    local engineRagdoll = ply:GetRagdollEntity() -- assert(engineRagdoll == ply.context.ragdoll) -- false
+    local engineRagdoll = ply:GetRagdollEntity() -- assert(engineRagdoll == ply.EPDAContext.ragdoll) -- false
     engineRagdoll:Remove()
 
-    if IsValid(ply.context.animator) then
-        ply.context.animator:Remove()
+    if IsValid(ply.EPDAContext.animator) then
+        ply.EPDAContext.animator:Remove()
     end
-    if IsValid(ply.context.follower) then
-        ply.context.follower:Remove()
+    if IsValid(ply.EPDAContext.follower) then
+        ply.EPDAContext.follower:Remove()
     end
 end)
 
@@ -122,9 +122,9 @@ hook.Add("PlayerSpawn", "PlayerSpawn_EPDA_LifeCycle", function(ply, transition)
 
     ply:SetModelScale(1, 0)
 
-    if not ply.context then return end
-    if IsValid(ply.context.ragdoll) then
-        ply.context.ragdoll:Remove()
+    if not ply.EPDAContext then return end
+    if IsValid(ply.EPDAContext.ragdoll) then
+        ply.EPDAContext.ragdoll:Remove()
     end
-    ply.context = nil
+    ply.EPDAContext = nil
 end)
